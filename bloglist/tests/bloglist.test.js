ca1d3c47c086f7d4d6d24be82dc67d8839d2c857
strahinja <strahinjaa.va@gmail.com>
2020-08-17 23:59:17 +0200

    4.7 tests

--- bloglist/tests/bloglist.test.js
+++ bloglist/tests/bloglist.test.js
@@ -1,10 +1,243 @@
 const listHelper = require('../utils/list_helper')
-const { TestResult } = require('@jest/types')
-const { exportAllDeclaration } = require('@babel/types')
 
 test('Dummy returns 1', () => {
-    const blogs = []
-    const result = listHelper(blogs)
+  const blogs = []
+  const result = listHelper.dummy(blogs)
 
-    expect(result).toBe(1)
+  expect(result).toBe(1)
+})
+
+describe('Total Likes', () => {
+  test('Empty array sums to 0', () => {
+    const blogs = []
+    const result = listHelper.totalLikes(blogs)
+
+    expect(result).toBe(0)
+  })
+  test('Sum of single blog is the amount of likes of that blog', () => {
+    const blogs = [{
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "me",
+      url: "http://www.thisisalink.com",
+      likes: "1000",
+      __v: 0
+    }]
+    const result = listHelper.totalLikes(blogs)
+
+    expect(result).toBe(Number(blogs[0].likes))
+  })
+  test('Sum of bigger list is calculated correctly', () => {
+    const blogs = [{
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "me",
+      url: "http://www.thisisalink.com",
+      likes: "1000",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "me",
+      url: "http://www.thisisalink.com",
+      likes: "10",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "me",
+      url: "http://www.thisisalink.com",
+      likes: "5",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "me",
+      url: "http://www.thisisalink.com",
+      likes: "100",
+      __v: 0
+    }]
+    const result = listHelper.totalLikes(blogs)
+
+    expect(result).toBe(1115)
+  })
+})
+
+describe('Favorite blog', () => {
+  test('Blog wit most likes', () => {
+    const blogs = [{
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "me",
+      url: "http://www.thisisalink.com",
+      likes: "1000",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "me",
+      url: "http://www.thisisalink.com",
+      likes: "10",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "me",
+      url: "http://www.thisisalink.com",
+      likes: "5",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "me",
+      url: "http://www.thisisalink.com",
+      likes: "50000",
+      __v: 0
+    }]
+
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual(blogs[3])
+
+  })
+  test('Best blogger', () => {
+    const blogs = [{
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "Strahinja",
+      url: "http://www.thisisalink.com",
+      likes: "1000",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "me",
+      url: "http://www.thisisalink.com",
+      likes: "1000",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "Strahinja",
+      url: "http://www.thisisalink.com",
+      likes: "1000",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "Strahinja",
+      url: "http://www.thisisalink.com",
+      likes: "1000",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "Marko",
+      url: "http://www.thisisalink.com",
+      likes: "10",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "Marko",
+      url: "http://www.thisisalink.com",
+      likes: "5",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "Marko",
+      url: "http://www.thisisalink.com",
+      likes: "50000",
+      __v: 0
+    },{
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "Strahinja",
+      url: "http://www.thisisalink.com",
+      likes: "50000",
+      __v: 0
+    }]
+    const result = listHelper.mostBlogs(blogs)
+    expect(result).toEqual({ author: 'Strahinja', count: 4 })
+  })
+  test('most likes', () => {
+    const blogs = [{
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "Strahinja",
+      url: "http://www.thisisalink.com",
+      likes: "1000",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "me",
+      url: "http://www.thisisalink.com",
+      likes: "10",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "Strahinja",
+      url: "http://www.thisisalink.com",
+      likes: "1000",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "Strahinja",
+      url: "http://www.thisisalink.com",
+      likes: "1000",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "Marko",
+      url: "http://www.thisisalink.com",
+      likes: "100",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "Marko",
+      url: "http://www.thisisalink.com",
+      likes: "100",
+      __v: 0
+    },
+    {
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "Marko",
+      url: "http://www.thisisalink.com",
+      likes: "100",
+      __v: 0
+    },{
+      _id: "5f31c78231564ce78565d0a5",
+      title: "A test",
+      author: "Strahinja",
+      url: "http://www.thisisalink.com",
+      likes: "1000",
+      __v: 0
+    }]
+    // strahinja: 4000 marko: 300 me: 10
+    const result = listHelper.mostLikes(blogs)
+    expect(result).toEqual({ author: 'Strahinja', total: 4000 })
+  })
 })
--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -1,5 +1,40 @@
+const _ = require('lodash')
 const dummy = (blogs) => {
   return 1
 }
 
-module.exports = dummy
+const totalLikes = (blogs) => {
+  const sumReducer = (sum, object) => sum + Number(object.likes)
+  return blogs.reduce(sumReducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const favoriteReducer = (initialBlog, object) => {
+    if (Number(initialBlog.likes) < Number(object.likes))
+      initialBlog = object
+    return initialBlog
+  }
+  return blogs.reduce(favoriteReducer, blogs[0])
+}
+
+const mostBlogs = (blogs) => {
+  let list = []
+  let obj = _.countBy(blogs, 'author')
+  Object.keys(obj).map(key => list.push({ author: key, count: obj[key] }))
+  return _.maxBy(list, 'count')
+}
+
+const mostLikes = (blogs) => {
+  const obj = _.groupBy(blogs, 'author')
+  const sumReducer = (init, obj) => init + Number(obj.likes)
+  return _.max(Object.keys(obj).map( key => { return { author: key, total: obj[key].reduce(sumReducer,0) } }))
+}
+
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
